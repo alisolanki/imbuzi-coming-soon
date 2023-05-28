9db109989ab4e5ae78eebb34d30c4090c766fb59
Ali Solanki <alisolanki123@gmail.com>
2023-05-29 00:18:37 +0530

    fix form back again

--- js/main.js
+++ js/main.js
@@ -5,68 +5,68 @@
     [ Validate ]*/
   var input = $(".validate-input .input100");
 
-  $("#newsletter-form").on("submit", function (e) {
-    var check = true;
+  // $("#newsletter-form").on("submit", function (e) {
+  //   var check = true;
 
-    e.preventDefault();
+  //   e.preventDefault();
 
-    var emailId = $("#newsletter-form input").val();
+  //   var emailId = $("#newsletter-form input").val();
 
-    for (var i = 0; i < input.length; i++) {
-      if (validate(input[i]) == false) {
-        showValidate(input[i]);
-        check = false;
-      }
-    }
-    if (check) {
-      // jquery ajax submit form
-      $.ajax({
-        url: "https://imbuzi-api.vercel.app/api/newsletter",
-        type: "POST",
-        data: { email: emailId },
-        dataType: "json",
-        accept: "application/json",
-        beforeSend: function () {
-          $("#newsletter-form .loading").show();
-        },
-        success: function (response) {
-          $("#newsletter-form .loading").hide();
-          window.location.href = "https://forms.gle/3W7otSEMTdirtnKK6";
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-          console.log(jqXHR, textStatus, errorThrown);
-          $("#newsletter-form .loading").hide();
-        },
-      });
-    }
+  //   for (var i = 0; i < input.length; i++) {
+  //     if (validate(input[i]) == false) {
+  //       showValidate(input[i]);
+  //       check = false;
+  //     }
+  //   }
+  //   if (check) {
+  //     // jquery ajax submit form
+  //     $.ajax({
+  //       url: "https://imbuzi-api.vercel.app/api/newsletter",
+  //       type: "POST",
+  //       data: { email: emailId },
+  //       dataType: "json",
+  //       accept: "application/json",
+  //       beforeSend: function () {
+  //         $("#newsletter-form .loading").show();
+  //       },
+  //       success: function (response) {
+  //         $("#newsletter-form .loading").hide();
+  //         window.location.href = "https://forms.gle/3W7otSEMTdirtnKK6";
+  //       },
+  //       error: function (jqXHR, textStatus, errorThrown) {
+  //         console.log(jqXHR, textStatus, errorThrown);
+  //         $("#newsletter-form .loading").hide();
+  //       },
+  //     });
+  //   }
 
-    return check;
-  });
+  //   return check;
+  // });
 
-  $(".validate-form .input100").each(function () {
-    $(this).focus(function () {
-      //    hideValidate(this);
-    });
-  });
+  // $(".validate-form .input100").each(function () {
+  //   $(this).focus(function () {
+  //     //    hideValidate(this);
+  //   });
+  // });
 
-  function validate(input) {
-    if ($(input).attr("type") == "email" || $(input).attr("name") == "email") {
-      if (
-        $(input)
-          .val()
-          .trim()
-          .match(
-            /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
-          ) == null
-      ) {
-        return false;
-      }
-    } else {
-      if ($(input).val().trim() == "") {
-        return false;
-      }
-    }
-  }
+  // function validate(input) {
+  //   if ($(input).attr("type") == "email" || $(input).attr("name") == "email") {
+  //     if (
+  //       $(input)
+  //         .val()
+  //         .trim()
+  //         .match(
+  //           /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+  //         ) == null
+  //     ) {
+  //       return false;
+  //     }
+  //   } else {
+  //     if ($(input).val().trim() == "") {
+  //       return false;
+  //     }
+  //   }
+  // }
 
   function showValidate(input) {
     var thisAlert = $(input).parent();
